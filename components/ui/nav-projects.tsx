9a39a44f60245f1dfcdf0b1b9a5eb5d00f86baa8
Ranjit Parmar <parmarranjit7972@gmail.com>
2026-05-07 19:43:37 +0530

    [Refactor] Update icon imports in nav-projects component to use direct paths

--- components/ui/nav-projects.tsx
+++ components/ui/nav-projects.tsx
@@ -1,7 +1,4 @@
 import Image from "next/image";
-import instagram_img from "@/public/instagram_icon.png";
-import facebook_img from "@/public/facebook_icon.png";
-import youtube_img from "@/public/youtube_icon.png";
 import {
   SidebarGroup,
   SidebarGroupLabel,
@@ -15,20 +12,37 @@ const projects = [
     name: "Instagram",
     url: "#",
     icon: (
-      <Image src={instagram_img} width={25} height={25} alt="Instagram Logo" />
+      <Image
+        src="/instagram_icon.png"
+        width={25}
+        height={25}
+        alt="Instagram Logo"
+      />
     ),
   },
   {
     name: "Facebook",
     url: "#",
     icon: (
-      <Image src={facebook_img} width={25} height={25} alt="Facebook Logo" />
+      <Image
+        src="/facebook_icon.png"
+        width={25}
+        height={25}
+        alt="Facebook Logo"
+      />
     ),
   },
   {
     name: "Youtube",
     url: "#",
-    icon: <Image src={youtube_img} width={25} height={25} alt="Youtube Logo" />,
+    icon: (
+      <Image
+        src="/youtube_icon.png"
+        width={25}
+        height={25}
+        alt="Youtube Logo"
+      />
+    ),
   },
 ];
 
